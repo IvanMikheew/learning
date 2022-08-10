67274c53b7c7b9feb867a59657fd2fba48923241
Ivan Mikheev <drowjajka@gmail.com>
2022-08-10 17:03:09 +0700

    Добавил Урок 4 и урок 5 на половину

--- JS/js.js
+++ JS/js.js
@@ -1,3 +1,4 @@
+// itProger урок 3
 console.log("Сообщение");
 console.info("Сообщение");
 let number = + prompt("Введите число", "1");
@@ -6,3 +7,11 @@ if (isNaN(number)) {
 }
 console.warn("Предупреждение");
 let neww;
+
+// itProger Урок 4
+const number1 = 5; //Такую переменную нельзя переопределить 
+
+// itProger Урок 5
+let number2 = 10;
+console.log(`Вычитание: ${number1 - number2}`); 
+console.log("Сложение: " + (number1 + number2));
